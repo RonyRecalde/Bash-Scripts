2daa66bb7189ba7e1919e19c198e2da002b7dcda
RonyRecalde <rony_recalde@hotmail.com>
2024-07-18 12:13:31 -0300

    Update crear-scripts.sh

--- Scripts para Iniciantes/crear-scripts.sh
+++ Scripts para Iniciantes/crear-scripts.sh
@@ -8,7 +8,7 @@ read -p "Digite el nombre de sus nuevo archivo: " nombre
 #Usamos el operador > para crear el nuevo archivo nombrado con el valor de la variable $nombre
 > $nombre
 # Al nuevo archivo creado, asignamos el contenido de la variable $header (esto permite que el nuevo archivo sea considerado como un script bash)
-echo "header" > $nombre
+echo $header > $nombre
 #El comando chmod permite específicar los permisos de un archivo
 #Damos permisos para ejecutar el nuevo archivo usando el argumento +x
 chmod +x $nombre
